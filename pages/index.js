b4557939628cfcacebd904aabb20debef5b3d36e
Drew Osei <andrewowusu21@yahoo.com>
2022-01-12 15:25:14 +0000

    feat-configuring application wide styles for app (#1)
    Co-authored-by: Andy-Osei <andrew.owusu@novanest.net>

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
+import styles from "../styles/Home.module.scss";
+
 export default function Home() {
-  return <h3>This is the beginning of something awesome using Next.js</h3>;
+  return (
+    <h1 className={styles.header}>
+      This is the beginning of something awesome using Next.js
+    </h1>
+  );
 }
